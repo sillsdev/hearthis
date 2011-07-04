b7be750342ce22cf1829821e811667b6b258ffc3
John Hatton <hattonjohn@gmail.com>
2011-07-04 09:46:17 -0700

    Initial Checkin of empty project

---
+++ Copy Libaries.bat
@@ -0,0 +1,18 @@
+copy /Y ..\palaso\output\debug\palaso.dll lib\net2.0\
+copy /Y ..\palaso\output\debug\palaso.xml lib\net2.0\
+copy /Y ..\palaso\output\debug\palaso.pdb lib\net2.0\
+
+copy /Y ..\palaso\output\debug\palasouiwindowsforms.dll  lib\net2.0
+copy /Y ..\palaso\output\debug\palasouiwindowsforms.xml  lib\net2.0
+copy /Y ..\palaso\output\debug\palasouiwindowsforms.pdb  lib\net2.0
+
+
+copy /Y ..\palaso\output\debug\palaso.testutilities.dll lib\net2.0
+copy /Y ..\palaso\output\debug\palaso.testutilities.xml lib\net2.0
+copy /Y ..\palaso\output\debug\palaso.testutilities.pdb net2.0
+
+copy /Y ..\palaso\output\debug\palaso.*  output\debug
+copy /Y ..\palaso\output\debug\palaso.testutilities.*  output\debug
+copy /Y ..\palaso\output\debug\palasouiwindowsforms.*  output\debug
+
+pause
---
+++ src/HearThisTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HearThisTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SIL")]
+[assembly: AssemblyProduct("HearThisTests")]
+[assembly: AssemblyCopyright("Copyright © SIL 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf62edb9-f63b-46f2-89dc-1c5c156e02bc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
