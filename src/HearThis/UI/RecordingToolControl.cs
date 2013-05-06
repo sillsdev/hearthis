7852ffcdd51565303a9a2156549a57e97dca9d6f
Gordon Martin <gordon-soft_martin@sil.org>
2013-05-06 14:17:37 -0700

    One more 'using' and close branch

--- src/HearThis/UI/RecordingToolControl.cs
+++ src/HearThis/UI/RecordingToolControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using HearThis.Properties;
 using HearThis.Publishing;
 using HearThis.Script;
 using L10NSharp;
