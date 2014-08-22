5ebd51a02803a4bd2bf888f9e72747a39e9c01e3
Tom Bogle <tom_bogle@sil.org>
2014-08-22 11:14:21 -0400

    Cleaned up PublishingModel tests. Need to see if they will work on TeamCity.

--- src/HearThisTests/PublishingModelTests.cs
+++ src/HearThisTests/PublishingModelTests.cs
@@ -13,62 +13,54 @@ namespace HearThisTests
 		// At one point this test was ignored because it failed on the (TeamCity) server.
 		// I (JohnT) changed it to this category so it can be used on developer machines.
 		// We don't recall how or why it fails on TeamCity, or even know for sure that it still does.
-		// It MIGHT be something to do with whether Paratext is installed, or whether a progress dialog can be shown.
-		[Category("SkipOnTeamCity")]
-		public void Publish_PublishRootPathHasOneNewDirectory_DirectoryCreated()
+		// It MIGHT be something to do with whether Paratext is installed.
+		//[Category("SkipOnTeamCity")]
+		public void Publish_PublishRootPathDoesNotExist_UsesDefaultLocation()
 		{
 			var m = new PublishingModel("foo");
 			var newRandomPart = Guid.NewGuid().ToString();
 			m.PublishRootPath = Path.Combine(Path.GetTempPath(), newRandomPart);
-			var progress = new StringBuilderProgress();
-			m.Publish(progress);
-			Assert.IsFalse(progress.ErrorEncountered);
+			m.Publish(new NullProgress());
+			Assert.AreEqual(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "HearThis-foo"), m.PublishThisProjectPath);
+			Assert.IsTrue(m.PublishThisProjectPath.StartsWith(m.PublishRootPath));
 			Assert.IsTrue(Directory.Exists(m.PublishRootPath));
+			Assert.IsTrue(Directory.Exists(m.PublishThisProjectPath));
 		}
 
 		[Test]
 		// At one point this test was ignored because it failed on the (TeamCity) server.
 		// I (JohnT) changed it to this category so it can be used on developer machines.
 		// We don't recall how or why it fails on TeamCity, or even know for sure that it still does.
-		// It MIGHT be something to do with whether Paratext is installed, or whether a progress dialog can be shown.
-		[Category("SkipOnTeamCity")]
-		public void Publish_PublishRootPathHasMoreThanOneNewDirectory_DirectoryCreated()
+		// It MIGHT be something to do with whether Paratext is installed.
+		//[Category("SkipOnTeamCity")]
+		public void Publish_CustomPublishRootPathExists_UsesCustomLocation()
 		{
 			var m = new PublishingModel("foo");
-			m.PublishRootPath = "c:/1/2/3";
+			var customPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+			Directory.CreateDirectory(customPath);
+			m.PublishRootPath = customPath;
 			m.Publish(new NullProgress());
+			Assert.AreEqual(Path.Combine(customPath, "HearThis-foo"), m.PublishThisProjectPath);
+			Assert.IsTrue(m.PublishThisProjectPath.StartsWith(m.PublishRootPath));
 			Assert.IsTrue(Directory.Exists(m.PublishRootPath));
+			Assert.IsTrue(Directory.Exists(m.PublishThisProjectPath));
 		}
-
-		[Test]
-		// At one point this test was ignored because it failed on the (TeamCity) server.
-		// I (JohnT) changed it to this category so it can be used on developer machines.
-		// We don't recall how or why it fails on TeamCity, or even know for sure that it still does.
-		// It MIGHT be something to do with whether Paratext is installed, or whether a progress dialog can be shown.
-		[Category("SkipOnTeamCity")]
-		public void Publish_PublishRootPathIsNull_PublishRootPathIsAtSomeDefaultPlace()
-		{
-			var m = new PublishingModel("foo");
-			m.PublishRootPath = null;
-			m.Publish(new NullProgress());
-			Assert.IsFalse(string.IsNullOrEmpty(m.PublishRootPath));
-			Assert.IsTrue(Directory.Exists(m.PublishRootPath));
-		}
-
 
 		[Test]
 		// At one point this test was ignored because it was thought that it might fail on the (TeamCity) server.
 		// I (JohnT) changed it to this category so it can be used on developer machines.
 		// We don't recall how or why it might fail on TeamCity, or even know for sure that it ever did.
-		// It MIGHT be something to do with whether Paratext is installed, or whether a progress dialog can be shown.
-		[Category("SkipOnTeamCity")]
+		// It MIGHT be something to do with whether Paratext is installed.
+		//[Category("SkipOnTeamCity")]
 		public void Publish_PublishRootPathIsNull_PublishThisProjectPathUnderneathMyDocuments()
 		{
 			var m = new PublishingModel("foo");
 			m.PublishRootPath = null;
 			m.Publish(new NullProgress());
-			Assert.IsTrue(m.PublishThisProjectPath.StartsWith(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)), m.PublishThisProjectPath);
-			Assert.IsTrue(m.PublishThisProjectPath.EndsWith("HearThis-foo"), m.PublishThisProjectPath);
+			Assert.AreEqual(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "HearThis-foo"), m.PublishThisProjectPath);
+			Assert.IsTrue(m.PublishThisProjectPath.StartsWith(m.PublishRootPath));
+			Assert.IsTrue(Directory.Exists(m.PublishRootPath));
+			Assert.IsTrue(Directory.Exists(m.PublishThisProjectPath));
 		}
 	}
 }
