e785bad0af179d3f2c9f26e68fb5541379714018
Tom Bogle <tom_bogle@sil.org>
2016-12-20 17:12:45 -0500

    Work-in-progress: utility to distinguish between punctuation and whitespace changes and actual word changes.

--- src/HearThis/Utils/Utils.cs
+++ src/HearThis/Utils/Utils.cs
@@ -8,14 +8,25 @@
 #endregion
 // --------------------------------------------------------------------------------------------
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace HearThis
 {
 	public static class Utils
 	{
+		//static readonly Regex s_replacePunctuationAndWhitespaceRegex = new Regex(@"\b[\w']+\b", RegexOptions.Compiled);
+		static readonly Regex s_replacePunctuationAndWhitespaceRegex = new Regex(@"[^\w']+", RegexOptions.Compiled);
+
 		public static string CreateDirectory(params string[] pathparts)
 		{
 			return Directory.CreateDirectory(Path.Combine(pathparts)).FullName;
+		}
+
+		public static bool AreWordsIdentical(string str1, string str2)
+		{
+			str1 = s_replacePunctuationAndWhitespaceRegex.Replace(str1, " ").Trim();
+			str2 = s_replacePunctuationAndWhitespaceRegex.Replace(str2, " ").Trim();
+			return str1 == str2;
 		}
 	}
 }
---
+++ src/HearThisTests/UtilsTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HearThis;
+using NUnit.Framework;
+
+namespace HearThisTests
+{
+	[TestFixture]
+	class UtilsTests
+	{
+		[Test]
+		public void AreWordsIdentical_Identical_ReturnsTrue()
+		{
+			Assert.IsTrue(Utils.AreWordsIdentical("The cat", "The cat"));
+		}
+
+		[TestCase("The cat", "The  cat")]
+		[TestCase("The  cat", "The cat")]
+		[TestCase("   The  cat ", " The cat")]
+		public void AreWordsIdentical_IdenticalExceptWhiteSpace_ReturnsTrue(string str1, string str2)
+		{
+			Assert.IsTrue(Utils.AreWordsIdentical(str1, str2));
+		}
+
+		[TestCase("The. cat", "The; cat")]
+		[TestCase("This is a cat", "This is a cat.")]
+		[TestCase("\"This is a cat\"", "This is a cat")]
+		public void AreWordsIdentical_IdenticalExceptPunctuation_ReturnsTrue(string str1, string str2)
+		{
+			Assert.IsTrue(Utils.AreWordsIdentical(str1, str2));
+		}
+
+		[TestCase("The cat's meow", "The cats meow.")]
+		[TestCase("The cats' meow", "The cats meow.")]
+		[TestCase("The cat's meow", "The cats' meow.")]
+		public void AreWordsIdentical_IdenticalExceptApostrophe_ReturnsFalse(string str1, string str2)
+		{
+			Assert.IsFalse(Utils.AreWordsIdentical(str1, str2));
+		}
+
+		[Test]
+		public void AreWordsIdentical_DifferentWords_ReturnsFalse()
+		{
+			Assert.IsFalse(Utils.AreWordsIdentical("The cat", "The dog"));
+		}
+
+		[TestCase("Cats & dogs are strange bed-fellows", "Cats & dogs are strange bedfellows")]
+		[TestCase("Cats & dogs are strange bed-fellows", "Cats & dogs are strange bed_fellows")]
+		[TestCase("Cats&dogs are strange.", "Cats & dogs are strange.")]
+		public void AreWordsIdentical_DifferentWordMedialPunctuation_ReturnsFalse(string str1, string str2)
+		{
+			Assert.IsFalse(Utils.AreWordsIdentical(str1, str2));
+		}
+	}
+}
