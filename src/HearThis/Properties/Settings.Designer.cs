015bc5542aa89f8bd8e273e67112ec88c98d2d6b
tom_bogle@sil.org <tom_bogle@sil.org>
2018-04-24 12:58:02 -0400

    Made "navigation labels" option off by default.

--- src/HearThis/Properties/Settings.Designer.cs
+++ src/HearThis/Properties/Settings.Designer.cs
@@ -393,7 +393,7 @@ namespace HearThis.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool DisplayNavigationButtonLabels {
             get {
                 return ((bool)(this["DisplayNavigationButtonLabels"]));
