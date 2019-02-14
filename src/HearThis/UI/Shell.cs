816cc8f5707c60cc86956a7bc53e906cc8a795c9
tom_bogle@sil.org <tom_bogle@sil.org>
2019-02-14 13:19:55 -0500

    Improvements to HearThis Settings Protection password notice
    (Depends on libpalaso PR 789)

--- src/HearThis/UI/Shell.cs
+++ src/HearThis/UI/Shell.cs
@@ -687,6 +687,7 @@ namespace HearThis.UI
 
 		private void supportToolStripMenuItem_Click(object sender, EventArgs e)
 		{
+			// This should be kept in sync with the appSetting "ProductSupportUrl" in app.config.
 			Process.Start(@"https://community.scripture.software.sil.org/c/hearthis");
 		}
 	}
