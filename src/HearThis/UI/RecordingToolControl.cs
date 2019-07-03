0983a685c566d86ed8016120a7e78fd65243a707
tom_bogle@sil.org <tom_bogle@sil.org>
2019-07-03 17:02:12 -0400

    Made the recording device indicator (on the main window) work in sync with the recording controls in RecordInPartsDlg .

--- src/HearThis/UI/RecordingToolControl.cs
+++ src/HearThis/UI/RecordingToolControl.cs
@@ -1080,6 +1080,7 @@ namespace HearThis.UI
 					_project.SelectedScriptBlock);
 				dlg.TextToRecord = scriptLine.Text;
 				dlg.RecordingDevice = _audioButtonsControl.RecordingDevice;
+				dlg.RecordingDeviceIndicator = recordingDeviceButton1;
 				dlg.ContextForAnalytics = _audioButtonsControl.ContextForAnalytics;
 				dlg.VernacularFont = new Font(scriptLine.FontName, scriptLine.FontSize * _scriptControl.ZoomFactor);
 				if (dlg.ShowDialog(this) == DialogResult.OK)
@@ -1088,6 +1089,7 @@ namespace HearThis.UI
 					OnSoundFileCreated(this, new EventArgs());
 				}
 			}
+			recordingDeviceButton1.Recorder = _audioButtonsControl.Recorder;
 		}
 
 		public void SetClauseSeparators(string clauseBreakCharacters)
