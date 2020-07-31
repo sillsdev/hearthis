a84e20e669a4de4601da86036e9f558e122da591
tom_bogle@sil.org <tom_bogle@sil.org>
2020-07-31 12:41:16 -0400

    Changed to use SDK -style projects. Upgraded SIL Dlls to latest beta. Upgraded NAudio.
    [WIP] msbuild fails to copy irrklag DLL from SIL.Media

--- src/HearThisTests/Properties/AssemblyInfo.cs
+++ src/HearThisTests/Properties/AssemblyInfo.cs
@@ -1,17 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HearThisTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SIL")]
 [assembly: AssemblyProduct("HearThisTests")]
-[assembly: AssemblyCopyright("Copyright © SIL 2011-2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,13 +12,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cf62edb9-f63b-46f2-89dc-1c5c156e02bc")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-//these are set dynamically by the build process; the numbers here are ignored.
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
