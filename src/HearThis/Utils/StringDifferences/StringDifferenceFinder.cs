18706c90334d1bd929942998e435c916438d0cf8
tombogle <tom_bogle@sil.org>
2022-02-28 16:38:47 -0500

    [WIP] This was an attempt to reduce the amount of code we have to maintain in HearThis by using the Diffplex library to compute string differences. I had/have
    some hope that this approach might also result in somewhat more useful diffs. But there were questions about how well Diffplex would handle data with surrogate pairs and the additional code to handle rejoining words, and making sure diacritics stayed with their base characters was proving non-trivial. Then I found what appears to be a fairly major bug/limitation, and I decided to abandon this. If that bug is fixed, it might be worth considering this approach in the future.

--- src/HearThis/Utils/StringDifferences/StringDifferenceFinder.cs
+++ src/HearThis/Utils/StringDifferences/StringDifferenceFinder.cs
@@ -9,11 +9,8 @@
 // --------------------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Text;
-using SIL.Extensions;
 using static Icu.Character;
-using static System.Char;
 
 namespace HearThis.StringDifferences
 {
@@ -92,331 +89,153 @@ namespace HearThis.StringDifferences
 			AllCharactersInWord = Int32.MaxValue,
 		}
 
-		private class CharacterComparisonState
+		private static CharactersToKeepTogether GetCharactersToKeepWith(int ch)
 		{
-			private Func<CharactersToKeepTogether, bool> NeedToRemovePreviouslyAddedCharacter { get; set; }
+			// ENHANCE: Implement correct logic for Hangul syllables
 
-			public bool KeepWholeWordsTogether { get; private set; }
+			if (ch == '\u200d') // Zero-width Joiner
+				return CharactersToKeepTogether.PrecedingAndFollowingBaseCharacter;
 
-			private int _charactersToRemoveIfMismatchFound;
-
-			private bool _matched = true;
-
-			private int _charactersInCurrentWord;
-
-			public int CharactersToRemove => _matched ? 0 : _charactersToRemoveIfMismatchFound;
-
-			public CharacterComparisonState(Func<CharactersToKeepTogether, bool> needToRemovePreviouslyAddedCharacter)
+			switch (GetCharType(ch))
 			{
-				NeedToRemovePreviouslyAddedCharacter = needToRemovePreviouslyAddedCharacter;
-			}
-
-			public void Reset(Func<CharactersToKeepTogether, bool> needToRemovePreviouslyAddedCharacter)
-			{
-				_matched = true;
-				_charactersToRemoveIfMismatchFound = 0;
-				_charactersInCurrentWord = 0;
-				NeedToRemovePreviouslyAddedCharacter = needToRemovePreviouslyAddedCharacter;
-			}
-
-			public bool DoCharactersMatch(string origStr, int o, string newStr, int n, out bool surrogate)
-			{
-				Debug.Assert(_matched, "Once a mismatch is found, no more characters should be tested until Reset is called.");
-
-				int chOrig = origStr[o];
-				if (IsHighSurrogate((char)chOrig))
-				{
-					surrogate = true;
-					chOrig = ConvertToUtf32(origStr, o);
-				}
-				else if (IsLowSurrogate((char)chOrig))
-				{
-					surrogate = true;
-					chOrig = ConvertToUtf32(origStr, o - 1);
-				}
-				else
-				{
-					surrogate = false;
-				}
-
-				int chNew = newStr[n];
-				if (IsHighSurrogate((char)chNew))
-					chNew = ConvertToUtf32(newStr, n);
-				else if (IsLowSurrogate((char)chNew))
-					chNew = ConvertToUtf32(newStr, n - 1);
-
-				var b = (CharactersToKeepTogether)Math.Max((int)GetCharactersToKeepWith(chOrig),
-					(int)GetCharactersToKeepWith(chNew));
-				KeepWholeWordsTogether |= b == CharactersToKeepTogether.AllCharactersInWord;
-
-				_matched = chOrig == chNew;
-
-				if (_matched)
-				{
-					if (origStr.IsLikelyWordForming(o))
-						_charactersInCurrentWord++;
-					else
-						_charactersInCurrentWord = 0;
-
-					if (!KeepWholeWordsTogether)
+				// REVIEW: It's possible we'd want to treat modifier letters in a similar fashion, but
+				// they do not form combined graphemes, and can sometimes modify the following letter
+				// rather than the preceding one.
+				//case UCharCategory.MODIFIER_LETTER:
+				//	break;
+				case UCharCategory.NON_SPACING_MARK:
+					switch (GetIntPropertyValue(ch, UProperty.CANONICAL_COMBINING_CLASS))
 					{
-						if (b == CharactersToKeepTogether.None)
-							_charactersToRemoveIfMismatchFound = 0;
-						else
-							_charactersToRemoveIfMismatchFound += (int)b;
+						// TODO: Write unit tests and handle additional combining classes.
+						// TODO: case 1: (Note that a few of these are most likely to be used in Latin script and could return
+						// PrecedingBaseCharacter, but most are for complex Asian scripts, so some other character is likely to
+						// return AllCharactersInWord. I'm leaning toward PrecedingBaseCharacter, but it might be good to find
+						// some real examples.
+						case 6: // Han Reading Combining Class (Vietnamese alternate reading marks)
+						// TODO:	case 7:
+						// TODO:	case 8:
+						// TODO: 10...199 (fixed position class)
+						case 9: // Virama
+							return CharactersToKeepTogether.AllCharactersInWord;
+						case 233: // Double_Below
+						case 234: // Double_Above
+							return CharactersToKeepTogether.PrecedingAndFollowingBaseCharacter;
 					}
-				}
-				else if (KeepWholeWordsTogether)
-				{
-					if (origStr.IsLikelyWordForming(o))
-						_charactersToRemoveIfMismatchFound = _charactersInCurrentWord;
-					else
-						_charactersToRemoveIfMismatchFound = 0;
-				}
-				else
-				{
-					if (NeedToRemovePreviouslyAddedCharacter(b))
-						_charactersToRemoveIfMismatchFound++;
-				}
-
-				return _matched;
+					return CharactersToKeepTogether.PrecedingBaseCharacter;
+				// REVIEW: These are seldom (if ever) used in normal (e.g., Scripture) text. Given
+				// the complexity in figuring out the number of base characters, possibly best to
+				// not support them at all here. But if they are used in a complex script where
+				// they are likely to apply to more than one "base" character, it will probably
+				// be the case that one of those characters will return AllCharactersInWord, and
+				// then it won't matter.
+				case UCharCategory.ENCLOSING_MARK: // See http://unicode.org/L2/L2003/03026-enclosing-marks.htm
+					return CharactersToKeepTogether.PrecedingBaseCharacter;
+				case UCharCategory.COMBINING_SPACING_MARK:
+					return CharactersToKeepTogether.AllCharactersInWord;
 			}
 
-			private static CharactersToKeepTogether GetCharactersToKeepWith(int ch)
-			{
-				// ENHANCE: Implement correct logic for Hangul syllables
-
-				if (ch == '\u200d') // Zero-width Joiner
-					return CharactersToKeepTogether.PrecedingAndFollowingBaseCharacter;
-
-				switch (GetCharType(ch))
-				{
-					// REVIEW: It's possible we'd want to treat modifier letters in a similar fashion, but
-					// they do not form combined graphemes, and can sometimes modify the following letter
-					// rather than the preceding one.
-					//case UCharCategory.MODIFIER_LETTER:
-					//	break;
-					case UCharCategory.NON_SPACING_MARK:
-						switch (GetIntPropertyValue(ch, UProperty.CANONICAL_COMBINING_CLASS))
-						{
-							// TODO: Write unit tests and handle additional combining classes.
-							case 6: // Han Reading Combining Class (Vietnamese alternate reading marks)
-							// TODO:	case 7:
-							// TODO:	case 8:
-							// TODO: 10...199 (fixed position class)
-							case 9: // Virama
-								return CharactersToKeepTogether.AllCharactersInWord;
-							case 233: // Double_Below
-							case 234: // Double_Above
-								return CharactersToKeepTogether.PrecedingAndFollowingBaseCharacter;
-						}
-						return CharactersToKeepTogether.PrecedingBaseCharacter;
-					// REVIEW: These are seldom (if ever) used in normal (e.g., Scripture) text. Given
-					// the complexity in figuring out the number of base characters, possibly best to
-					// not support them at all here. But if they are used in a complex script where
-					// they are likely to apply to more than one "base" character, it will probably
-					// be the case that one of those characters will return AllCharactersInWord, and
-					// then it won't matter.
-					case UCharCategory.ENCLOSING_MARK: // See http://unicode.org/L2/L2003/03026-enclosing-marks.htm
-						return CharactersToKeepTogether.PrecedingBaseCharacter;
-					case UCharCategory.COMBINING_SPACING_MARK:
-						return CharactersToKeepTogether.AllCharactersInWord;
-				}
-
-				return 0;
-			}
+			return 0;
 		}
-		
+
+		private static bool HasReasonableWordBreaks(string s, List<char> whitespaceChars)
+		{
+			var wsCharCount = 0;
+			foreach (var c in s)
+			{
+				if (char.IsWhiteSpace(c))
+				{
+					wsCharCount++;
+					if (!whitespaceChars.Contains(c))
+						whitespaceChars.Add(c);
+				}
+			}
+
+			return wsCharCount > .08 * s.Length;
+		}
+
 		/// <summary>
 		/// Works through the two strings to find segments that are the same and different.
 		/// </summary>
 		private IEnumerable<StringDifferenceSegment> ComputeDifferences(string origStr, string newStr)
 		{
-			// Step 1: Look for and return an initial common substring
-			var bldr = new StringBuilder();
-			int o = 0;
-			int n = 0;
-
-			// In the case of either PrecedingBaseCharacter or
-			// PrecedingAndFollowingBaseCharacter, we will have already added
-			// the preceding base character, so we need to remove it .
-			bool MustRemovePrevBase(CharactersToKeepTogether c) => c != CharactersToKeepTogether.None;
-			var ccState = new CharacterComparisonState(MustRemovePrevBase);
-
-			for (; o < origStr.Length && n < newStr.Length; o++, n++)
+			int lastDiffOrigEnd = 0;
+			var whitespaceChars = new List<char>();
+			if (HasReasonableWordBreaks(origStr, whitespaceChars) || HasReasonableWordBreaks(newStr, whitespaceChars))
 			{
-				if (ccState.DoCharactersMatch(origStr, o, newStr, n, out var surrogate))
+				int lastDiffNewEnd = 0;
+				var result = DiffPlex.Differ.Instance.CreateWordDiffs(origStr, newStr, false, false, whitespaceChars.ToArray());
+				foreach (var diffBlock in result.DiffBlocks)
 				{
-					bldr.Append(origStr[o]);
-					if (surrogate)
+					if (diffBlock.DeleteStartA > lastDiffOrigEnd)
+						yield return new StringDifferenceSegment(DifferenceType.Same, origStr.Substring(lastDiffOrigEnd, diffBlock.DeleteStartA));
+
+					var sb = new StringBuilder();
+
+					if (diffBlock.DeleteCountA > 0)
 					{
-						bldr.Append(origStr[++o]);
-						n++;
+						for (int w = 0; w < diffBlock.DeleteCountA; w++)
+						{
+							while (!whitespaceChars.Contains(origStr[++lastDiffOrigEnd]))
+								sb.Append(origStr[lastDiffOrigEnd]);
+							while (whitespaceChars.Contains(origStr[lastDiffOrigEnd]))
+								sb.Append(origStr[lastDiffOrigEnd++]);
+						}
+						yield return new StringDifferenceSegment(DifferenceType.Deletion, sb.ToString());
+					}
+
+					if (diffBlock.InsertCountB > 0)
+					{
+						sb = new StringBuilder();
+						for (int w = 0; w < diffBlock.InsertCountB; w++)
+						{
+							while (!whitespaceChars.Contains(newStr[++lastDiffNewEnd]))
+								sb.Append(newStr[lastDiffNewEnd]);
+							while (whitespaceChars.Contains(newStr[lastDiffNewEnd]))
+								sb.Append(newStr[lastDiffNewEnd++]);
+						}
+						yield return new StringDifferenceSegment(DifferenceType.Addition, sb.ToString());
 					}
 				}
-				else
-				{
-					if (ccState.CharactersToRemove > 0)
-					{
-						bldr.Remove(bldr.Length - ccState.CharactersToRemove, ccState.CharactersToRemove);
-						n -= ccState.CharactersToRemove;
-						o -= ccState.CharactersToRemove;
-					}
-					break;
-				}
-			}
-
-			if (bldr.Length > 0)
-				yield return new StringDifferenceSegment(DifferenceType.Same, bldr.ToString());
-
-			// Step 2: If we've run out of characters in one of the two strings (i.e., they started
-			// off identically, but text was chopped off the original version or added to the end
-			// of the new version), return an addition or deletion representing the trailing
-			// difference.
-			origStr = origStr.Substring(o);
-			newStr = newStr.Substring(n);
-
-			if (origStr.Length == 0)
-			{
-				if (newStr.Length > 0)
-					yield return new StringDifferenceSegment(DifferenceType.Addition, newStr);
-				yield break;
-			}
-			if (newStr.Length == 0)
-			{
-				yield return new StringDifferenceSegment(DifferenceType.Deletion, origStr);
-				yield break;
-			}
-
-			// Step 3: Look for a trailing common substring (don't return it yet)
-			bldr.Clear();
-			// Since we are working backward from the end of the string, we only need to
-			// remove a character if this is a diacritic that would have joined the
-			// previous character in the string to the following one (which we've already
-			// processed).
-			bool MustRemoveFollowingBase(CharactersToKeepTogether c) =>
-				c == CharactersToKeepTogether.PrecedingAndFollowingBaseCharacter;
-			ccState.Reset(MustRemoveFollowingBase);
-
-			for (o = origStr.Length - 1, n = newStr.Length - 1;
-			     o >= 0 && n >= 0; o--, n--)
-			{
-				if (ccState.DoCharactersMatch(origStr, o, newStr, n, out var surrogate))
-				{
-					bldr.Insert(0, origStr[o]);
-					if (surrogate)
-					{
-						bldr.Insert(0, origStr[--o]);
-						n--;
-					}
-				}
-				else
-				{
-					if (ccState.CharactersToRemove > 0)
-					{
-						bldr.Remove(0, ccState.CharactersToRemove);
-						n -= ccState.CharactersToRemove;
-						o -= ccState.CharactersToRemove;
-					}
-					break;
-				}
-			}
-
-			// Step 4: Within the middle chunk that is different, find the longest common
-			// substring. Note: If we have already found viramas or letters that combine to form
-			// ligatures (or totally different glyphs) we pass in 1.0 as the last param, which
-			// prevents it from even trying to find a common substring that is not a whole word
-			// (because in this kind of script it is virtually impossible to know which characters
-			// will combine how in order to be able to visually indicate to the user what part of a
-			// word changed). Otherwise, we tweak this method's default "magic number" of 15%
-			// similarity because for our purposes we generally want to avoid returning partial
-			// words as matches. But for scriptio continua or highly agglutinative languages, we
-			// do want to allow for them. It's possible that this will need to be parameterized
-			// so that the user can control the sensitivity of this (probably using a slider
-			// control), but it would be hard to describe and they would really have to play with
-			// it to figure out what works best. In any case, the visual color-coding we provide
-			// is designed to be a help, so if it's a little too helpful at times and not quite
-			// helpful enough at others, it's probably not the end of the world.
-			var common = origStr.Substring(0, origStr.Length - bldr.Length).GetLongestUsefulCommonSubstring(
-				newStr.Substring(0, newStr.Length - bldr.Length), out _, ccState.KeepWholeWordsTogether ? 1.0 :.20);
-
-			if (common.Length > 1)
-			{
-				var iOrig = origStr.IndexOf(common);
-				var iNew = newStr.IndexOf(common);
-				if (iNew < iOrig)
-					ExpandCommonSubstringIfNeeded(ref iNew, ref iOrig, ref common, newStr, origStr);
-				else
-					ExpandCommonSubstringIfNeeded(ref iOrig, ref iNew, ref common, origStr, newStr);
-
-				// Step 4.A.i: Return the leading addition and/or deletion
-				if (iOrig > 0)
-					yield return new StringDifferenceSegment(DifferenceType.Deletion, origStr.Substring(0, iOrig));
-				
-				if (iNew > 0)
-					yield return new StringDifferenceSegment(DifferenceType.Addition, newStr.Substring(0, iNew));
-
-				// Step 4.A.ii: Return the common substring
-				yield return new StringDifferenceSegment(DifferenceType.Same, common);
-
-				// Step 4.A.iii: Return the trailing addition and/or deletion
-				var iDelStart = iOrig + common.Length;
-				var delLength = origStr.Length - bldr.Length - iDelStart;
-				if (delLength > 0)
-					yield return new StringDifferenceSegment(DifferenceType.Deletion, origStr.Substring(iDelStart, delLength));
-
-				var iAddStart = iNew + common.Length;
-				var addLength = newStr.Length - bldr.Length - iAddStart;
-				if (addLength > 0)
-					yield return new StringDifferenceSegment(DifferenceType.Addition, newStr.Substring(iAddStart, addLength));
+				if (lastDiffOrigEnd < origStr.Length)
+					yield return new StringDifferenceSegment(DifferenceType.Same, origStr.Substring(lastDiffOrigEnd));
 			}
 			else
 			{
-				// Step 4.B: Nothing found in common between the beginning of the first difference
-				// and the end of the last difference. Just return the addition and/or deletion
-				if (o >= 0)
-					yield return new StringDifferenceSegment(DifferenceType.Deletion, origStr.Substring(0, o + 1));
-				if (n >= 0)
-					yield return new StringDifferenceSegment(DifferenceType.Addition, newStr.Substring(0, n + 1));
-			}
+				var result = DiffPlex.Differ.Instance.CreateCharacterDiffs(origStr, newStr, false, false);
+				foreach (var diffBlock in result.DiffBlocks)
+				{
+					int o = 0;
+					//int n = 0;
 
-			// Step 5: Now return the trailing common substring found in step 3. 
-			if (bldr.Length > 0)
-				yield return new StringDifferenceSegment(DifferenceType.Same, bldr.ToString());
-		}
+					int ch = origStr[diffBlock.DeleteStartA];
+					//if (IsHighSurrogate((char)ch))
+					//	ch = ConvertToUtf32(origStr, diffBlock.DeleteStartA);
+					//else if (IsLowSurrogate((char)ch))
+					//	ch = ConvertToUtf32(origStr, diffBlock.DeleteStartA - ++o);
 
-		/// <summary>
-		/// This helper method adjusts the results from GetLongestUsefulCommonSubstring (which gets
-		/// whole words if possible) to include the leading and or trailing characters from the
-		/// shorter of the two strings if they fully match the corresponding leading/trailing
-		/// characters in the longer string. This prevents the possibility of (an) extra
-		/// addition(s) in the shorter string that could have been more logically represented as an
-		/// addition only in the longer string.
-		/// </summary>
-		/// <param name="iShort">Index to the start of the common substring in shortStr</param>
-		/// <param name="iLong">Index to the start of the common substring in longStr</param>
-		/// <param name="common">The longest "useful" common substring between shortStr and
-		/// longStr, which this method will expand if possible</param>
-		/// <param name="shortStr">The string whose leading portion (before the common substring)
-		/// is shorter</param>
-		/// <param name="longStr">The string whose leading portion (before the common substring)
-		/// is longer</param>
-		private void ExpandCommonSubstringIfNeeded(ref int iShort, ref int iLong, ref string common,
-			string shortStr, string longStr)
-		{
-			Debug.Assert(iShort <= iLong);
-			if (shortStr.Substring(0, iShort) == longStr.Substring(iLong - iShort, iShort))
-			{
-				common = common.Insert(0, shortStr.Substring(0, iShort));
-				iLong -= iShort;
-				iShort = 0;
-			}
+					if (diffBlock.DeleteCountA > 0 && GetCharactersToKeepWith(ch) != CharactersToKeepTogether.None)
+						o++;
+					else if (diffBlock.InsertCountB > 0)
+					{
+						ch = newStr[diffBlock.InsertStartB];
+						if (GetCharactersToKeepWith(ch) != CharactersToKeepTogether.None)
+							o++;
+					}
 
-			var shortRemaining = shortStr.Substring(iShort + common.Length);
-			if (longStr.Length >= iLong + common.Length + shortRemaining.Length &&
-				shortRemaining == longStr.Substring(iLong + common.Length, shortRemaining.Length))
-			{
-				common += shortRemaining;
+					if (diffBlock.DeleteStartA > lastDiffOrigEnd)
+						yield return new StringDifferenceSegment(DifferenceType.Same, origStr.Substring(lastDiffOrigEnd, diffBlock.DeleteStartA - o));
+
+					if (diffBlock.DeleteCountA > 0)
+						yield return new StringDifferenceSegment(DifferenceType.Deletion, origStr.Substring(diffBlock.DeleteStartA - o, diffBlock.DeleteCountA));
+
+					if (diffBlock.InsertCountB > 0)
+						yield return new StringDifferenceSegment(DifferenceType.Addition, newStr.Substring(diffBlock.InsertStartB - o, diffBlock.InsertCountB));
+
+					lastDiffOrigEnd = diffBlock.DeleteStartA + diffBlock.DeleteCountA;
+				}
+
+				if (lastDiffOrigEnd < origStr.Length)
+					yield return new StringDifferenceSegment(DifferenceType.Same, origStr.Substring(lastDiffOrigEnd));
 			}
 		}
 	}
