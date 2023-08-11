3d9b4b6b413b0536de2ba400d12e093aa90a9540
tombogle <tom_bogle@sil.org>
2023-08-11 12:40:09 -0400

    Added Platform property to build file so it doesn't have to be passed in.

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,7 +1,7 @@
 call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 pushd .
-MSbuild /target:Build /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:Platform=x64 /property:BUILD_NUMBER="*.*.0.789" /property:Minor="1"
-MSbuild /target:Test;SignIfPossible;MakeDownloadPointers;ConvertReleaseNotesToHtml /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:Platform=x64 /property:BUILD_NUMBER="*.*.0.789" /property:Minor="1"
+MSbuild /target:Build /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.0.789" /property:Minor="1"
+MSbuild /target:Test;SignIfPossible;MakeDownloadPointers;ConvertReleaseNotesToHtml /property:teamcity_build_checkoutDir=..\  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="*.*.0.789" /property:Minor="1"
 popd
 PAUSE
