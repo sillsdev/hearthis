83947658e53eeae49116aa5f73129092a1dc25b6
ocienna <110152473+ocienna@users.noreply.github.com>
2024-03-06 13:43:56 -0500

    Update OpusEncoder.cs

--- src/HearThis/Publishing/OpusEncoder.cs
+++ src/HearThis/Publishing/OpusEncoder.cs
@@ -5,22 +5,33 @@ using SIL.Progress;
 
 namespace HearThis.Publishing
 {
-	/// <summary>
-	/// this is just to ensure that megavoice gets the precise bit/rate it wants
-	/// </summary>
-	public class OpusEncoder : IAudioEncoder
-	{
-		public void Encode(string sourcePath, string destPathWithoutExtension, IProgress progress)
-		{
-			progress.WriteMessage("   " + LocalizationManager.GetString("OpusEncoder.Progress", "Converting to opus format", "Appears in progress indicator"));
-			string args = $"--bitrate 64 \"{sourcePath}\" \"{destPathWithoutExtension}.opus\"";
-			string exePath = FileLocationUtilities.GetFileDistributedWithApplication("opusenc.1", "opusenc.exe");
-			progress.WriteVerbose(exePath + " " + args);
-			var result = CommandLineRunner.Run(exePath, args, "", 60 * 10, progress);
-			if (result.StandardError.Contains("FAIL"))
-				progress.WriteError(result.StandardError);
-		}
+    /// <summary>
+    /// This class is used to encode audio files to the Opus format.
+    /// </summary>
+    public class OpusEncoder : IAudioEncoder
+    {
+        public void Encode(string sourcePath, string destPathWithoutExtension, IProgress progress)
+        {
+            // Display a progress message
+            progress.WriteMessage("   " + LocalizationManager.GetString("OpusEncoder.Progress", "Converting to opus format", "Appears in progress indicator"));
 
-		public string FormatName => "opus";
-	}
+            // Construct the arguments for opusenc
+            string args = $"--bitrate 64 \"{sourcePath}\" \"{destPathWithoutExtension}.opus\"";
+
+            // Get the path to the opusenc executable
+            string exePath = "path/to/opusenc"; // Replace "path/to/opusenc" with the actual path to opusenc
+
+            // Write the command and arguments to the progress
+            progress.WriteVerbose(exePath + " " + args);
+
+            // Run opusenc with the specified arguments
+            var result = CommandLineRunner.Run(exePath, args, "", 60 * 10, progress);
+
+            // Check for errors
+            if (result.StandardError.Contains("FAIL"))
+                progress.WriteError(result.StandardError);
+        }
+
+        public string FormatName => "opus";
+    }
 }
